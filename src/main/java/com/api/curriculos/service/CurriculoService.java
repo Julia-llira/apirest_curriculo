559df64756059803591039ca7d1189f910d9ede4
Julia-llira <julia.00000847237@unicap.br>
2023-12-14 00:02:34 -0300

    correcao

--- src/main/java/com/api/curriculos/service/CurriculoService.java
+++ src/main/java/com/api/curriculos/service/CurriculoService.java
@@ -2,10 +2,8 @@ package com.api.curriculos.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.api.curriculos.model.Curriculo;
 import com.api.curriculos.repository.CurriculoRepository;
-
 import java.util.List;
 import java.util.Optional;
 
